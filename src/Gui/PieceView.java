93c6ce9ea2963a4df59b36c8e4a65823067f460d
Senko <153408399+MikoUwU24@users.noreply.github.com>
2025-04-06 19:50:46 +0700

    add GameController

--- src/Gui/PieceView.java
+++ src/Gui/PieceView.java
@@ -2,25 +2,33 @@ package Gui;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.input.MouseEvent;
 
-public class PieceView extends StackPane {
+public class PieceView extends GridPane {
     private ImageView imageView;
     private String pieceType;
     private String color;
+    private int col;
+    private int row;
 
     private static final int TILE_SIZE = 80;
 
     public PieceView(String pieceType, String color, int col, int row) {
         this.pieceType = pieceType.toLowerCase();
         this.color = color.toLowerCase();
+        this.col = col;
+        this.row = row;
         initializePiece();
         setPosition(col, row);
         setupClickHandler();
     }
 
     private void initializePiece() {
-        String imagePath = String.format("/media/%s%s.png", color.charAt(0), capitalize(pieceType));
+        String imagePath = String.format("/media/%s%s.png",
+                color.charAt(0), capitalize(pieceType));
 
         try {
             Image image = new Image(getClass().getResourceAsStream(imagePath));
@@ -33,7 +41,7 @@ public class PieceView extends StackPane {
         }
     }
 
-    private void setPosition(int col, int row) {
+    void setPosition(int col, int row) {
         setTranslateX(col * TILE_SIZE);
         setTranslateY(row * TILE_SIZE);
     }
@@ -41,7 +49,6 @@ public class PieceView extends StackPane {
     private void setupClickHandler() {
         setOnMouseClicked(event -> {
             System.out.println(color + " " + pieceType + " clicked!");
-            // Optionally: notify controller, highlight, or toggle selection
             setStyle("-fx-border-color: yellow; -fx-border-width: 2px;");
         });
     }
@@ -51,11 +58,15 @@ public class PieceView extends StackPane {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
-    public String getPieceType() {
-        return pieceType;
-    }
-
-    public String getColor() {
-        return color;
+    public String getPieceType() { return pieceType; }
+    public String getColor() { return color; }
+    public int getCol() { return col; }
+    public int getRow() { return row; }
+    public void highlight(boolean enabled) {
+        if(enabled) {
+            setStyle("-fx-border-color: yellow; -fx-border-width: 2px;");
+        } else {
+            setStyle("");
+        }
     }
 }
